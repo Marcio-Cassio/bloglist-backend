bd32dd61adf23c1cb7c800b9fb56c9d4adfc4b60
Marcio <marcioscontact@gmail.com>
2026-02-22 21:07:35 -0300

    Finished exercises (4.15-4.23) part4 implementing token auth and user management

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,56 +1,89 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+blogsRouter.post('/', middleware.userExtractor, async (request, response, next) => {
+  try {
+    const body = request.body
 
-  if (!body.title || !body.url) {
-    return response.status(400).json({ error: 'title or url missing'})
-  } 
-  
-  const blog = new Blog(body)
+    if (!body.title || !body.url) {
+      return response.status(400).json({ error: 'title or url missing' })
+    }
 
-  const result = await blog.save()
-  response.status(201).json(result)
+    const user = request.user
+
+    const blog = new Blog({
+      ...body,
+      user: user._id,
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const result = await Blog.findByIdAndDelete(request.params.id)
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
 
-  if (!result) {
-    return response.status(404).end()
+    if (!blog) {
+      return response.status(404).end()
+    }
+
+    const user = request.user
+
+    if (!blog.user || blog.user.toString() !== user._id.toString()) {
+      return response.status(403).json({ error: 'only the creator can delete a blog' })
+    }
+
+    await Blog.findByIdAndDelete(request.params.id)
+    await User.findByIdAndUpdate(blog.user, { $pull: { blogs: blog._id } }) 
+
+    response.status(204).end()
+  } catch (error) {
+    next(error)
   }
-
-  response.status(204).end()
 })
 
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const body = request.body
 
-  const updatedData = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
+    const updatedData = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      updatedData,
+      { returnDocument: 'after', runValidators: true, context: 'query' }
+    )
+
+    if (!updatedBlog) {
+      return response.status(404).end()
+    }
+
+    response.json(updatedBlog)
+  } catch (error) {
+    next(error)
   }
-
-  const updatedBlog = await Blog.findByIdAndUpdate(
-    request.params.id,
-    updatedData,
-    { returnDocument: 'after', runValidators: true, context: 'query' }
-  )
-
-  if (!updatedBlog) {
-    return response.status(404).end()
-  }
-
-  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,12 +1,23 @@
-const { test, describe, beforeEach, after } = require('node:test')
+const { test, describe, beforeEach, after, before } = require('node:test')
 const assert = require('node:assert')
 const supertest = require('supertest')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
 
 const app = require('../app')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+before(async () => {
+  if (mongoose.connection.readyState !== 1) {
+    await new Promise((resolve, reject) => {
+      mongoose.connection.once('connected', resolve)
+      mongoose.connection.once('error', reject)
+    })
+  }
+})
 
 const initialBlogs = [
   {
@@ -19,21 +30,60 @@ const initialBlogs = [
     title: 'Second blog',
     author: 'Tester Two',
     url: 'http://example.com/2',
-    likes: 5
+    likes: 5,
   },
 ]
 
 describe('when there are initially some blogs saved', () => {
+  let token
+
   beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = await new User({
+      username: 'root',
+      name: 'Superuser',
+      passwordHash,
+    }).save()
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'sekret' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    token = loginResponse.body.token
+    assert.ok(token)
+
+    const blogsWithUser = initialBlogs.map((blog) => ({
+      ...blog,
+      user: user._id,
+    }))
+
+    await Blog.insertMany(blogsWithUser)
+  })
+
+  test('adding a blog fails with 401 if token is not provided', async () => {
+    const newBlog = {
+      title: 'No token blog',
+      author: 'No Token',
+      url: 'http://example.com/notoken',
+      likes: 1,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initialBlogs.length)
   })
 
   test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+    await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
   })
 
   test('all blogs are returned', async () => {
@@ -43,7 +93,6 @@ describe('when there are initially some blogs saved', () => {
 
   test('the unique identifier property of the blog posts is named id', async () => {
     const response = await api.get('/api/blogs')
-
     const blog = response.body[0]
     assert.ok(blog.id)
     assert.strictEqual(blog._id, undefined)
@@ -53,11 +102,12 @@ describe('when there are initially some blogs saved', () => {
     const newBlog = {
       title: 'A new blog',
       author: 'New Author',
-      url: 'http://example.com/new'
+      url: 'http://example.com/new',
     }
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -65,7 +115,7 @@ describe('when there are initially some blogs saved', () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, initialBlogs.length + 1)
 
-    const titles = response.body.map(b => b.title)
+    const titles = response.body.map((b) => b.title)
     assert(titles.includes('A new blog'))
   })
 
@@ -78,6 +128,7 @@ describe('when there are initially some blogs saved', () => {
 
     const response = await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -94,6 +145,7 @@ describe('when there are initially some blogs saved', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(400)
 
@@ -110,6 +162,7 @@ describe('when there are initially some blogs saved', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(400)
 
@@ -123,13 +176,13 @@ describe('when there are initially some blogs saved', () => {
 
     await api
       .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(204)
 
     const blogsAtEnd = await api.get('/api/blogs')
-
     assert.strictEqual(blogsAtEnd.body.length, blogsAtStart.body.length - 1)
 
-    const titles = blogsAtEnd.body.map(b => b.title)
+    const titles = blogsAtEnd.body.map((b) => b.title)
     assert(!titles.includes(blogToDelete.title))
   })
 
@@ -149,6 +202,28 @@ describe('when there are initially some blogs saved', () => {
       .expect('Content-Type', /application\/json/)
 
     assert.strictEqual(response.body.likes, blogToUpdate.likes + 1)
+  })
+
+  test('deleting a blog fails with 403 if user is not the creator', async () => {
+    await api
+      .post('/api/users')
+      .send({ username: 'other', name: 'Other User', password: 'sekret2' })
+      .expect(201)
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'other', password: 'sekret2' })
+      .expect(200)
+
+    const otherToken = loginResponse.body.token
+
+    const blogsAtStart = await api.get('/api/blogs')
+    const blogToDelete = blogsAtStart.body[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${otherToken}`)
+      .expect(403)
   })
 })
 
